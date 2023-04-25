5267eccd5073761cce029bd89ee1585f204202b5
Aniket Singh <aniketsingh252002@gmail.com>
2023-04-25 09:16:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// const nav = document.querySelector('nav');
+// const menuBtn = document.querySelector('.menu-btn');
+// const closeBtn = document.querySelector('.close-btn');
+
+// menuBtn.addEventListener('click', () => {
+//   nav.classList.add('show');
+// });
+
+// closeBtn.addEventListener('click', () => {
+//   nav.classList.remove('show');
+// });
