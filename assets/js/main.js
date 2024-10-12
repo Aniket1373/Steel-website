08871812e2cedd53a64186f43db51f2f8ae9c4c2
Aniket Singh <87801473+Aniket1373@users.noreply.github.com>
2024-10-12 18:47:37 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: Lumia
-* Updated: Mar 10 2023 with Bootstrap v5.2.3
-* Template URL: https://bootstrapmade.com/lumia-bootstrap-business-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
